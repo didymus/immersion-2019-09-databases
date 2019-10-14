5cb6c03bcb78cfb504b8aa85c0ab9205f5a8cf42
thomasmudge <alephcult@gmail.com>
2019-10-14 15:27:21 -0500

    Reworked `index.js` from `models`

--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,56 @@
-const users = require('./users');
-const messages = require('./messages');
-const { connection } = require('../db');
+//const users = require('./users');
+//const messages = require('./messages');
+const { db } = require('../db');
 
 module.exports = {
-  users,
-  messages,
+  users: {
+    get: (callback) => {
+      const qs = 'select text, roomname from messages';
+      db.query(queryString, (err, results) => {
+        if(err){
+          console.log('Error: ', err);
+        } else {
+          callback(results);
+        }
+      });
+    },
+
+    post: (user) => {
+      //console.log(user);
+      const text = message.message.replace(/(\')/g, '\'\'');
+      console.log(text);
+      const qs = `insert into users (username) values ('${user.username}')`;
+      db.query(qs, (err) => {
+        if(err){
+          console.log('Error: ', err);
+        }
+      });
+    },
+  },
+  messages: {
+    // get all messages
+    get: (callback) => {
+      const qs = 'select text, roomname from messages';
+      db.query(qs, (err, results) => {
+        if(err){
+          console.log('Error: ', err);
+        } else {
+          callback(results);
+        }
+      });
+    },
+
+    // insert message into db
+    post: (message) => {
+      //console.log(message);
+      const text = message.message.replace(/(\')/g, '\'\'');
+      console.log(text);
+      const qs = `insert into messages (text, roomname) values ('${text}', '${message.roomname}')`;
+      db.query(qs, (err) => {
+        if(err){
+          console.log('Error: ', err);
+        }
+      });
+    },
+  },
 };
